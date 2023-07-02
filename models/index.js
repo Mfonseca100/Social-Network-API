117166a62b2d7d9b4851bc9bf138a1b348f7be33
Edith <rissafonseca@yahoo.com>
2023-07-01 22:21:57 -0500

    fixed lowercase issue for routes

--- models/index.js
+++ models/index.js
@@ -1,5 +1,5 @@
 
-const Thoughts = require('./Thoughts');
-const Users = require('./Users');
+const Thoughts = require('./Thought');
+const Users = require('./User');
 
 module.exports = { Users, Thoughts };
